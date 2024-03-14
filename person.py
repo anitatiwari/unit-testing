5abb475c2f81db861fd9c24e53d93e3709add17c
Anita Tiwari <85789858+anitatiwari@users.noreply.github.com>
2024-03-14 12:49:12 +0100

    Update person.py

--- person.py
+++ person.py
@@ -5,9 +5,9 @@ class Person:
         self.name.append(user_name)
         return len(self.name) - 1
 
-    def get_name(self, user_id):
-        if user_id >= len(self.name):
-            return 'There is no such user'
+   def get_name(self, user_id):
+        if user_id >= len(self.name) or user_id < 0:  # Modified condition
+            raise IndexError('Invalid user ID')  # Raise an exception for invalid user IDs
         else:
             return self.name[user_id]
 
